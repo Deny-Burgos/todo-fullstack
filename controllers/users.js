404d8881b8af5f856a9f8a6388502c133e04e5c8
Deny-Burgos <denyburgos1305@gmail.com>
2023-08-19 12:22:52 -0400

    aqui se guarda el usuario con contraseña encryptada en mongodb

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,26 @@
 const usersRouter = require('express').Router();
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
 
 usersRouter.post('/', async (request, response) => {
   const { name, email, password } = request.body;
+
   if (!name || !email || !password) {
     return response.status(400).json({ error: 'Todos los espacios son requeridos' });
   }
+
+  const salRounds = 10;
+  const passwordHash = await bcrypt.hash(password, salRounds);
+
+  const newUser = new User({
+    name,
+    email,
+    passwordHash,
+  });
+
+  const saveUser = await newUser.save();
+  console.log(saveUser);
+
 });
 
 module.exports = usersRouter;
