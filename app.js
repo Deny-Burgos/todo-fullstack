e0ac32655b3a489e8655d5ab5f1b626794de7fcd
Deny-Burgos <denyburgos1305@gmail.com>
2023-08-29 15:47:11 -0400

    fixed

--- app.js
+++ app.js
@@ -11,10 +11,11 @@ const loginRouter = require('./controllers/login');
 const todosRouter = require('./controllers/todos');
 const { userExtractor } = require('./middleware/auth');
 const logoutRouter = require('./controllers/logout');
+const { MONGO_URI } = require('./config');
 
 (async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI_DEV);
+    await mongoose.connect(MONGO_URI);
     console.log('Conecto a mongodb');
   } catch (error) {
     console.error(error);
